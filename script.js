eaa9363d9ccd433a828199adba21c4b1297db0dd
Mohi Eddine <161092073+Mohi03@users.noreply.github.com>
2025-05-21 00:05:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
         generateBtn.disabled = true;
         
         // Initial call to get scenarios
-        fetch('http://127.0.0.1:5000/gene', {
+        fetch('http://127.0.0.1:5000/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -182,7 +182,7 @@ document.addEventListener('DOMContentLoaded', function() {
         confirmLoadingIndicator.classList.remove('hidden');
         
         // Send scenarios for test generation
-        fetch('http://127.0.0.1:5000/gene', {
+        fetch('http://127.0.0.1:5000/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
